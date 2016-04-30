b402b3ae77ebded15cca1f23980c0498b8f05410
Kevin Burke <burke@shyp.com>
2016-04-29 20:35:09 -0700

    Add server example

---
+++ server/example_test.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/Shyp/rickover/rest"
+)
+
+type auther struct{}
+
+func (a *auther) Authorize(userId, token string) *rest.Error {
+	// Implement your auth scheme here.
+	return nil
+}
+
+func Example() {
+	// Get all server routes using your authorization handler, then listen on
+	// port 9090
+	handler := Get(&auther{})
+	http.ListenAndServe(":9090", handler)
+}
