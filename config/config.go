baa7af84a8525e09a7bb0056a0f6cd9fb8f3b403
Kevin Burke <burke@shyp.com>
2016-04-29 22:58:16 -0700

    0.19

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 )
 
-const Version = "0.18"
+const Version = "0.19"
 
 // GetInt loads the environment variable varName, converts it to an integer,
 // and returns that integer or an error.
